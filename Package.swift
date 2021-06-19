b99904b7809d63fbbcd33afa319ad5b99e1a54c7
Todd Bowden <toddhokie@gmail.com>
2021-06-19 12:40:45 -0400

    slv

--- Package.swift
+++ Package.swift
@@ -28,5 +28,6 @@ let package = Package(
         .testTarget(
             name: "TBFileManagerTests",
             dependencies: ["TBFileManager"]),
-    ]
+    ],
+    swiftLanguageVersions: [.v5])
 )
